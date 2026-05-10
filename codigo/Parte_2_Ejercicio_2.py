0c9261edcc90f472dd26a434f38c81a6e5a08e57
FacundoPiriz17 <facundo.piriz@correo.ucu.edu.uy>
2026-05-10 20:56:06 -0300

    minor fix

--- codigo/Parte_2_Ejercicio_2.py
+++ codigo/Parte_2_Ejercicio_2.py
@@ -1,14 +1,19 @@
 from Parte_2_Ejercicio_1 import simular_X
 
+
 def simular_muestra(n, cantidad_monedas=10):
-    return [simular_X(cantidad_monedas) for _ in range(n)]
+    muestra = []
+
+    for _ in range(n):
+        x = simular_X(cantidad_monedas)
+        muestra.append(x)
+
+    return muestra
 
 
 if __name__ == "__main__":
+    valores_n = [10**3, 10**4, 10**5, 10**6]
 
-    muestra = simular_muestra(10)
-    #muestra = simular_muestra(10**3)
-    #muestra2 = simular_muestra(10**4)
-    #muestra3 = simular_muestra(10**5)
-    #muestra4 = simular_muestra(10**6)
-    print(muestra)
+    for n in valores_n:
+        muestra = simular_muestra(n, 10)
+        print(f"n = {n} | tamaño de la muestra = {len(muestra)}")
